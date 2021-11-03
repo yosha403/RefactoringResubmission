c02ab3287fc0b8aa9120069ab63c91355cfe7841
Yosha Kunnummal <yoshakunnummal@rocketmortgage.com>
2021-11-03 08:40:05 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+
+namespace PigLatin
+{
+    class PigLatinTranslator
+    {
+        //static void Main(string[] args)
+        //{
+        //    string userInput = GetInput("Please input a word or sentence to translate to pig Latin");
+
+        //    string translation = ToPigLatin(userInput);
+        //    Console.WriteLine(translation);
+        //}
+
+        public string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine().ToLower().Trim();
+            return input;
+        }
+
+        public bool IsVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            //return c.ToString() == vowels.ToString();
+            return vowels.Contains(c);
+        }
+
+        public string ToPigLatin(string word)
+        {
+            word = word.ToLower().Trim();
+
+            //Split with no input it splits along white space
+            string[] sentence = word.Split();
+            string output = "";
+            foreach (string wo in sentence)
+            {
+                char[] specialChars = { '@', '.', '-', '$', '^', '&' };
+                bool hasSpecialChars = false;
+                foreach (char c in specialChars)
+                {
+                    if (wo.Contains(c))
+                    {
+                        Console.WriteLine("That word has special characters, we will return it as is");
+                        output += wo + " ";
+                        hasSpecialChars = true;
+                        break;
+                    }
+
+                    //foreach (char w in word)
+                    //{
+                    //    if (w == c)
+                    //    {
+                    //        return word;
+                    //    }
+                    //}
+
+                }
+
+                if (hasSpecialChars)
+                {
+                    continue;
+                }
+
+                bool noVowels = true;
+                foreach (char letter in wo)
+                {
+                    if (IsVowel(letter))
+                    {
+                        noVowels = false;
+                        break;
+                    }
+                }
+
+                if (noVowels)
+                {
+                    output += wo + " ";
+                    continue;
+                }
+
+                char firstLetter = wo[0];
+
+                if (IsVowel(firstLetter) == true)
+                {
+                    output += wo + "way ";
+                }
+                else
+                {
+                    //-1 is a universal sign for nothing was found or there was 
+                    int vowelIndex = -1;
+                    //Handle going through all the consonants
+                    for (int i = 0; i <= wo.Length; i++)
+                    {
+                        if (IsVowel(wo[i]) == true)
+                        {
+                            vowelIndex = i;
+                            break;
+                        }
+                    }
+
+                    string sub = wo.Substring(vowelIndex);
+                    string postFix = wo.Substring(0, vowelIndex);
+
+                    output += sub + postFix + "ay ";
+                }
+            }
+            return output.Trim();
+        }
+    }
+}
---
+++ TestPigLatin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PigLatin
+{
+    public class TestPigLatin
+    {
+        [Theory]
+        [InlineData("apple", "appleway")]
+        [InlineData("heck", "eckhay")]
+        [InlineData("strong", "ongstray")]
+        [InlineData("tommy@email.com", "tommy@email.com")]
+        [InlineData("aardvark", "aardvarkway")]
+        [InlineData("Tommy", "ommytay")]
+        [InlineData("gym", "gym")]
+        [InlineData("apple joy gym tommy@email.com strong", "appleway oyjay gym tommy@email.com ongstray")]
+
+        public void TestTranslate(string input, string expected)
+        {
+            //Arrange 
+            PigLatinTranslator p = new PigLatinTranslator();
+
+            //Act 
+            string actual = p.ToPigLatin(input);
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData('a', true)]
+        [InlineData('y', false)]
+        [InlineData('t', false)]
+        [InlineData('o', true)]
+        public void TestIsVowel(char letter, bool expected)
+        {
+            //Arrange 
+            PigLatinTranslator p = new PigLatinTranslator();
+
+            //Act
+            bool actual = p.IsVowel(letter);
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+    }
+}
